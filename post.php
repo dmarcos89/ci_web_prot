81abb76ee55116a5bab57c43985c202765f6505d
dmarcos89 <dmarcos1989@gmail.com>
2014-07-03 20:13:33 -0300

    Initial Commit

---
+++ post.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+
+<div class="full-width-image-2">
+   <!-- <h1 style="margin:0">Titulo del articulo</h1> -->
+         <!-- box content goes here if you want to put text or a button in here -->
+</div>
+
+ 
+ <div class="container">
+    
+    <br>
+
+    <div class="pull-left">
+        <img src="http://placeimg.com/30/30/any/grayscale" alt="">
+        <span>Nombre Usuario</span>
+    </div>
+    <div class="pull-right">
+        <a href=""> #Graffiti  </a>
+        <a href=""> #Tag2  </a>
+        <a href=""> #Tag3  </a>
+    </div>
+    <br>
+    <!-- <hr> -->
+
+    <h1 class="titlearticle">Titulo del articulo</h1>
+
+    <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam modi quod, fugiat ex earum impedit corporis, velit autem maxime eum provident facere, officiis reiciendis nobis possimus expedita laudantium. Beatae, deserunt? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis voluptatem, nihil delectus quisquam, iusto laborum magni, asperiores id ipsum tenetur vero? Dignissimos maiores repellat quaerat nobis commodi. Necessitatibus iure, nihil. </p>
+
+    <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam modi quod, fugiat ex earum impedit corporis, velit autem maxime eum provident facere, officiis reiciendis nobis possimus expedita laudantium. Beatae, deserunt? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis voluptatem, nihil delectus quisquam, iusto laborum magni, asperiores id ipsum tenetur vero? Dignissimos maiores repellat quaerat nobis commodi. Necessitatibus iure, nihil. </p>
+
+    <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam modi quod, fugiat ex earum impedit corporis, velit autem maxime eum provident facere, officiis reiciendis nobis possimus expedita laudantium. Beatae, deserunt? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis voluptatem, nihil delectus quisquam, iusto laborum magni, asperiores id ipsum tenetur vero? Dignissimos maiores repellat quaerat nobis commodi. Necessitatibus iure, nihil. </p>
+
+    <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam modi quod, fugiat ex earum impedit corporis, velit autem maxime eum provident facere, officiis reiciendis nobis possimus expedita laudantium. Beatae, deserunt? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis voluptatem, nihil delectus quisquam, iusto laborum magni, asperiores id ipsum tenetur vero? Dignissimos maiores repellat quaerat nobis commodi. Necessitatibus iure, nihil. </p>
+
+    <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam modi quod, fugiat ex earum impedit corporis, velit autem maxime eum provident facere, officiis reiciendis nobis possimus expedita laudantium. Beatae, deserunt? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis voluptatem, nihil delectus quisquam, iusto laborum magni, asperiores id ipsum tenetur vero? Dignissimos maiores repellat quaerat nobis commodi. Necessitatibus iure, nihil. </p>
+
+    
+    <br><br>
+    
+    <h3 class="centrado">Aportes</h3>
+
+    <br>
+    
+    <div class="pull-left">
+        <img src="http://placeimg.com/30/30/any/grayscale" alt="">
+        <span>Nombre Usuario</span>
+
+        <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione neque quaerat dolore? Aliquam natus eveniet ad veniam. Omnis ducimus soluta debitis, possimus dolores repudiandae officiis sed obcaecati impedit sint inventore. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione neque quaerat dolore? Aliquam natus eveniet ad veniam. Omnis ducimus soluta debitis, possimus dolores repudiandae officiis sed obcaecati impedit sint inventore.</p>
+        <hr>
+        <!-- <br><br> -->
+        
+    </div>
+    <hr>
+    
+
+    <div class="pull-left">
+        <img src="http://placeimg.com/30/30/any/grayscale" alt="">
+        <span>Nombre Usuario</span>
+
+        <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione neque quaerat dolore? Aliquam natus eveniet ad veniam. Omnis ducimus soluta debitis, possimus dolores repudiandae officiis sed obcaecati impedit sint inventore.</p>
+    
+        <div class="centrado">
+            <img src="img/post2.jpg" alt="" height=400>
+        </div>
+        <hr>
+        <!-- <br><br> -->
+    </div>
+
+
+
+    <div class="pull-left">
+        <img src="http://placeimg.com/30/30/any/grayscale" alt="">
+        <span>Nombre Usuario</span>
+
+        <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione neque quaerat dolore? Aliquam natus eveniet ad veniam. Omnis ducimus soluta debitis, possimus dolores repudiandae officiis sed obcaecati impedit sint inventore. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione neque quaerat dolore? Aliquam natus eveniet ad veniam. Omnis ducimus soluta debitis, possimus dolores repudiandae officiis sed obcaecati impedit sint inventore.</p>
+
+        <div class="centrado">
+            <iframe width="560" height="315" src="//www.youtube.com/embed/iaINvTIDQmQ" frameborder="0" allowfullscreen></iframe>
+
+        </div>
+        <hr>    
+        <!-- <br><br> -->
+        
+    </div>
+
+    <div class="pull-left">
+        <img src="http://placeimg.com/30/30/any/grayscale" alt="">
+        <span>Nombre Usuario</span>
+
+        <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione neque quaerat dolore? Aliquam natus eveniet ad veniam. Omnis ducimus soluta debitis, possimus dolores repudiandae officiis sed obcaecati impedit sint inventore. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione neque quaerat dolore? Aliquam natus eveniet ad veniam. Omnis ducimus soluta debitis, possimus dolores repudiandae officiis sed obcaecati impedit sint inventore.</p>
+        <hr>
+        
+    </div>
+
+    
+    
+    <div>
+        <h3 class="centrado">¡Aportar al posteo!</h3>
+        <br>
+        <textarea style="height:200px;" type="text" class="form-control" id="" placeholder="Escribe aquí tu aporte..."></textarea>
+        <a href="" class="btn btn-primary btn-block">Aportar</a>
+    </div>
+    
+
+
+    <div>
+        <h3 class="centrado">Compartir</h3>
+        
+        <div class="centrado">
+          <img src="img/share.png" alt="">
+        </div>
+    </div>
+        
+
+
+
+ </div>  
+    <!-- /.container -->
+
+
+    <!-- JavaScript -->
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+    
+
+</body>
+
+</html>
