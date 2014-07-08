2b7673af12b5361958d21d1d156035b199523d43
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2014-07-07 22:11:49 -0300

    Angular working OK

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,13 @@
-var myApp = angular.module('myApp', ['ngResource']);
+var myApp = angular.module('myApp', []);
 
-myApp.factory('restservice', function ($resource) {
+myApp.controller('PostsController', function($scope, $http) {
+      $scope.posts = [];
+      return $http.get('http://ciudadinvisible.herokuapp.com/posts.json').success(function(data) {
+        return $scope.posts = data;
+      });
+    });
+
+/*myApp.factory('restservice', function ($resource) {
     var source = $resource(
         "http://ciudadinvisible.herokuapp.com/posts.json/");
             // "http://rest-service.guides.spring.io/greeting/");
@@ -15,5 +22,4 @@ myApp.factory('restservice', function ($resource) {
 function AvengersCtrl($scope, $resource, restservice) {
     $scope.servicedata = [restservice];
     //this log shows the promise
-    console.log($scope.servicedata);
-}
+    console.log($scope.servicedata);*/
