d2a505dd02b0ae29ba114bba88f41b27cd952ea3
dmarcos89 <dmarcos1989@gmail.com>
2014-08-03 16:13:20 -0300

    Security module updated for Facebook

--- app/common/security.js
+++ app/common/security.js
@@ -2,14 +2,57 @@
 
 angular.module('Security',['facebook']);
 
-angular.module('Security').controller('LoginController', function($scope) {
-      $scope.isAuthenticated = false;
-      // $scope.isAuthenticated = true;
-    });
-
 
 angular.module('Security').config(['FacebookProvider', function(FacebookProvider) {
      // Here you could set your appId through the setAppId method and then initialize
      // or use the shortcut in the initialize method directly.
       FacebookProvider.init('494836457286098');
     }]);
+
+
+angular.module('Security').controller('LoginController', ['$scope', 'Facebook', function($scope, Facebook) {
+
+	$scope.isAuthenticated = false;
+	// $scope.isAuthenticated = true;
+
+	
+
+  // Here, usually you should watch for when Facebook is ready and loaded
+  $scope.$watch(function() {
+    return Facebook.isReady(); // This is for convenience, to notify if Facebook is loaded and ready to go.
+  }, function(newVal) {
+    $scope.facebookReady = true; // You might want to use this to disable/show/hide buttons and else
+  });
+
+  // From now on you can use the Facebook service just as Facebook api says
+  // Take into account that you will need $scope.$apply when inside a Facebook function's scope and not angular
+  $scope.login = function() {
+    Facebook.login(function(response) {
+      // Do something with response. Don't forget here you are on Facebook scope so use $scope.$apply
+    });
+  };
+
+  $scope.getLoginStatus = function() {
+    Facebook.getLoginStatus(function(response) {
+      if(response.status === 'connected') {
+        $scope.$apply(function() {
+          $scope.loggedIn = true;
+        });
+      }
+      else {
+        $scope.$apply(function() {
+          $scope.loggedIn = false;
+        });
+      }
+    });
+  };
+
+  $scope.me = function() {
+    Facebook.api('/me', function(response) {
+      $scope.$apply(function() {
+        // Here you could re-check for user status (just in case)
+        $scope.user = response;
+      });
+    });
+  };
+}]);
