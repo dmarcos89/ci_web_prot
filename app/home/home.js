939f0c601c3fd510c4530db13ecacea3f37a271b
dmarcos89 <dmarcos1989@gmail.com>
2014-08-21 21:29:41 -0300

    home html update

--- app/home/home.js
+++ app/home/home.js
@@ -1,3 +1,11 @@
+'use strict';
+
 angular.module('MainApp').controller('HomeController', function($scope) {
- $scope.message = "Esta es la home";
+  $scope.message = 'Esta es la home';
+
+  $scope.myInterval = 5000;
+  $scope.slides = [{image: 'http://lorempixel.com/1200/400/nature', text: 'Primera imagen del slider'}, {image: 'http://lorempixel.com/1200/400/', text: 'Segunda imagen'}];
+  
+
 });
+
