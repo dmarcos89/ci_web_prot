b0feb0c1cff66d15b94a80d3a7e8f11b6e8a8567
dmarcos89 <dmarcos1989@gmail.com>
2014-10-08 01:38:08 -0200

    twitter login test2

--- app/common/security.js
+++ app/common/security.js
@@ -257,7 +257,7 @@ angular.module('Security').controller('LoginController', ['$scope', '$rootScope'
             updateLoginVars(true,'TW',json.name);
 
             alert('Your name is '+ json.name);
-            alert(JSON.stringify(json));
+            alert(json);
             var data = {username: json.name, email: "email", first_name: json.name, last_name: json.lastname, twitter_id: json.id, avatar: photoUrl+$scope.facebookid };
             Login_Twitter.save(data, successPostCallback, errorCallback);
             
